2b554ba773f5ab5e4abf7c580961db56fb35b814
Deepak Kumar <deepakumar.avviare@gmail.com>
2024-01-13 14:11:58 +0530

    This is my first commit

---
+++ store/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.store, name="store"),
+    path('catogery/<slug:category_slug>/', views.store, name="products_by_category"),
+    path('catogery/<slug:category_slug>/<slug:product_slug>/', views.product_detail, name="product_detail"),
+    path('search/', views.search, name="search"),
+
+]
